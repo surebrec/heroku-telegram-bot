6dfa5d725546dba3a25898d02f7e33a0952b5320
Konstantin Pekov <pekov@compmechlab.ru>
2021-02-02 15:39:10 +0300

    + log

--- bot.py
+++ bot.py
@@ -14,13 +14,17 @@ bot = telebot.TeleBot(token)
 # some_api = some_api_lib.connect(some_api_token)
 #              ...
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  print(message.text)
-  bot.reply_to(message, "FUCK OFF")
-  
+    print(message.text)
+    bot.reply_to(message, "FUCK OFF")
+
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    print(f"{message}\n")
+    bot.reply_to(message, message.text)
+
   
 bot.polling()
